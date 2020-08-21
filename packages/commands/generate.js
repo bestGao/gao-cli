3f8a666766c2a24d05957a1e1ebffdcfaf289e21
gao <1055652678@qq.com>
2020-08-21 10:50:24 +0800

    fix

--- packages/commands/generate.js
+++ packages/commands/generate.js
@@ -1,6 +1,6 @@
 const { chalkLog } = require('./../lib/util')
 const axios = require('axios')
-const child_process = require('child_process')
+const { exec } = require('child_process')
 const path = require('path');
 const fs = require('fs')
 const shelljs = require('shelljs');
@@ -9,21 +9,31 @@ const { generateType } = require('./../init')
 const jarUrl = path.join(__dirname, '../../node_modules/@openapitools/openapi-generator-cli/bin/openapi-generator.jar')
 const genFolder = path.join(process.cwd(), './typings')
 
-try {
-    const stat = fs.statSync(genFolder)
-    if (stat.isDirectory()) {
-        chalkLog('scripts目录存在\n')
-    } else {
-        fs.mkdirSync(genFolder)
-        chalkLog('新建typings目录\n')
-    }
-    const { APIGROUP } = require(importFileUrl)
-    const url = APIGROUP[0]
-    // APIGROUP.for()
-    child_process.exec(`java -jar ${jarUrl} generate -g typescript-axios --skip-validate-spec -i ${url} -o "${genFolder}"`, { encoding: 'utf8' })
-    process.exit()
-} catch (err) {
-    chalkLog('获取typings目录报错\n')
-    fs.mkdirSync(genFolder)
-    process.exit(0)
-}
+fs.access(genFolder, fs.constants.F_OK, err => {
+    console.log(err)
+})
+
+// try {
+//     const stat = fs.statSync(genFolder)
+//     if (stat.isDirectory()) {
+//         chalkLog('scripts目录存在\n')
+//         chalkLog('执行')
+//         exec(`java -jar ${jarUrl} generate -g typescript-axios --skip-validate-spec -i ${url} -o "${genFolder}"`, { encoding: 'utf8' }, (error, stdout, stderr) => {
+//             chalkLog(error, stdout, stderr)
+//         })
+//         return false
+//     } else {
+//         fs.mkdirSync(genFolder)
+//         chalkLog('新建typings目录\n')
+//     }
+//     const { APIGROUP } = require(importFileUrl)
+//     const url = APIGROUP[0]
+//     // APIGROUP.for()
+
+//     // process.exit()
+//     return
+// } catch (err) {
+//     chalkLog('获取typings目录报错: \n', err)
+//     // fs.mkdirSync(genFolder)
+//     // process.exit()
+// }
--- packages/lib/constant.js
+++ packages/lib/constant.js
@@ -1,6 +1,6 @@
 const TypeObjs = {
     0: '生成typings 和 apis',
-    1: '生成typeings',
+    1: '生成typings',
     2: '生成apis'
 }
 
